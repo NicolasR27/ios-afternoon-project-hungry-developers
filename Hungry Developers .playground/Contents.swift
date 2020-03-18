f2a0d662fda53a44c5c417da8105ad8a06f1f274
Nicolas Rios <nickrios33@yahoo.com>
2020-03-17 22:44:18 -0700

    completed project

--- Hungry Developers .playground/Contents.swift
+++ Hungry Developers .playground/Contents.swift
@@ -49,3 +49,43 @@ class Developer {
     }
 }
 
+var spoon1 = Spoon()
+var spoon2 = Spoon()
+var spoon3 = Spoon()
+var spoon4 = Spoon()
+var spoon5 = Spoon()
+
+spoon1.index = 1
+spoon2.index = 2
+spoon3.index = 3
+spoon4.index = 4
+spoon5.index = 5
+
+let developer1 = Developer()
+let developer2 = Developer()
+let developer3 = Developer()
+let developer4 = Developer()
+let developer5 = Developer()
+
+var developerArray: [Developer] = []
+developerArray.append(contentsOf: [developer1, developer2, developer3, developer4, developer5])
+
+developer1.leftSpoon = spoon1
+developer1.rightSpoon = spoon2
+
+developer2.leftSpoon = spoon2
+developer2.rightSpoon = spoon3
+
+developer3.leftSpoon = spoon3
+developer3.rightSpoon = spoon4
+
+developer4.leftSpoon = spoon4
+developer4.rightSpoon = spoon5
+
+developer5.leftSpoon = spoon5
+developer5.rightSpoon = spoon1
+
+
+DispatchQueue.concurrentPerform(iterations: 5) { developerArray[$0].run()}
+    
+
